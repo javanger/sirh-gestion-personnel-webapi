e80aee62a89e7dbed240ec7c8331b598edda724b
Valentin <lefebvre.valentin76@gmail.com>
2018-04-10 12:47:04 +0200

    Valentin - Ajout requete departement et matricule

--- src/main/java/dev/sgp/controller/CollaborateurController.java
+++ src/main/java/dev/sgp/controller/CollaborateurController.java
@@ -3,22 +3,33 @@ package dev.sgp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.sgp.entite.Collaborateur;
 import dev.sgp.repository.CollaborateurRepository;
 
 @RestController
-@RequestMapping("/collaborateur")
 public class CollaborateurController {
 
 	@Autowired
 	private CollaborateurRepository collaborateurRepo;
 
-	@GetMapping
+	@RequestMapping(value = "/collaborateur", method = RequestMethod.GET)
 	public List<Collaborateur> listerexemples() {
 		return this.collaborateurRepo.findAll();
+	}
+
+	@RequestMapping(value = "/collaborateur", method = RequestMethod.GET, params = { "departement" })
+	public List<Collaborateur> findCollabByDepartement(@RequestParam("departement") Integer id) {
+		return collaborateurRepo.findCollabByDepartement(id);
+	}
+
+	@RequestMapping(value = "/collaborateur/{matricule}", method = RequestMethod.GET)
+	public Collaborateur findCollab(@PathVariable String matricule) {
+		return collaborateurRepo.findByMatricule(matricule);
 	}
 }
--- src/main/java/dev/sgp/repository/CollaborateurRepository.java
+++ src/main/java/dev/sgp/repository/CollaborateurRepository.java
@@ -1,9 +1,16 @@
 package dev.sgp.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import dev.sgp.entite.Collaborateur;
 
 public interface CollaborateurRepository extends JpaRepository<Collaborateur, Integer> {
 
+	Collaborateur findByMatricule(String matricule);
+
+	@Query("select c from Collaborateur c join c.departement dp where dp.id = ?1")
+	List<Collaborateur> findCollabByDepartement(Integer id);
 }
